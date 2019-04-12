ae8cfebb521e3409d3e22eefcd9f6fcc29e4d09d
Присяжный Александр Олегович <Prisyazhny.A.O@sberbank.ru>
2019-04-12 14:55:50 +0300

    Create markup and js functionality for saving name locally

--- public/chat.js
+++ public/chat.js
@@ -2,19 +2,48 @@
 var socket = io.connect('http://localhost:4000');
 
 // Query DOM
-var message = document.getElementsByClassName('message')[0],
-    handle = document.getElementsByClassName('handle')[0],
-    sendBtn = document.getElementsByClassName('send')[0],
-    output = document.getElementsByClassName('output')[0],
-    feedback = document.getElementsByClassName('feedback')[0];
+var message = document.getElementsByClassName('message-form__message')[0],
+    handle = document.getElementsByClassName('message-form__handle')[0],
+    sendBtn = document.getElementsByClassName('message-form__send')[0],
+    output = document.getElementsByClassName('chat-window__output')[0],
+    feedback = document.getElementsByClassName('chat-window__feedback')[0];
+    nameDisplay = document.getElementsByClassName('message-form__name-display')[0];
+
+var changeNameBtn;
+var name;
+
+function setName () {
+    if (!name) {
+        name = handle.value;
+    }
+    nameDisplay.innerHTML = '<p>' + name + '<button class="message-form__change-name">Изменить</button></p>';
+    handle.style.display = "none";
+    changeNameBtn = document.getElementsByClassName('message-form__change-name')[0];
+
+    changeNameBtn.addEventListener('click', function () {
+        handle.style.display = "block";
+        nameDisplay.innerHTML = '';
+        name = '';
+    });
+}
 
 // Emit events
 sendBtn.addEventListener('click', function () {
-    socket.emit('chat-message', {
-        message: message.value,
-        handle: handle.value
-    });
-    message.value = '';
+    if (handle.value && !message.value) {
+        setName();
+        console.log('Enter message');    
+    } else if (!handle.value && message.value) {
+        console.log('Enter name');
+    } else if (handle.value && message.value) {
+        socket.emit('chat-message', {
+            message: message.value,
+            handle: handle.value
+        });
+        setName();
+        message.value = '';
+    } else {
+        console.log('Enter name and message');
+    }
 });
 
 message.addEventListener('keypress', function () {
